46592be1fadbb6ff50ed5f780b50772b42837a5f
FaikCoskunCakir <fcoskuncakir@hotmail.com>
2023-10-28 15:03:32 +0300

    Initial Commit

---
+++ CICDOKR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDOKR
+//
+//  Created by Cakir, Faik on 28.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
